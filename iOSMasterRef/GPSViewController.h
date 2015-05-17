8afe617b87ec3ff7f88518dfcfb95721d72666c1
Bobby Nielsen <bobby.nielsen@creatorminds.dk>
2015-05-17 13:28:24 +0200

    All stories done

---
+++ iOSMasterRef/GPSViewController.h
@@ -0,0 +1,13 @@
+//
+//  GPSViewController.h
+//  iOSMasterRef
+//
+//  Created by Bobby Nielsen on 01/05/15.
+//  Copyright (c) 2015 CreatorMinds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPSViewController : UIViewController
+
+@end
---
+++ iOSMasterRef/GPSViewController.m
@@ -0,0 +1,13 @@
+//
+//  GPSViewController.m
+//  iOSMasterRef
+//
+//  Created by Bobby Nielsen on 01/05/15.
+//  Copyright (c) 2015 CreatorMinds. All rights reserved.
+//
+
+#import "GPSViewController.h"
+
+@implementation GPSViewController
+
+@end
